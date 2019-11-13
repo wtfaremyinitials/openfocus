40eb0e2bada59bcdefa103f58e867dc297647279
Will Franzen <wtfranzen@gmail.com>
2019-11-12 17:54:32 -0700

    WIP multi-transaction databases

---
+++ src/db.rs
@@ -0,0 +1,98 @@
+use regex::Regex;
+use std::fs::read_dir;
+///use std::fs::{read_dir, File};
+use std::path::PathBuf;
+use crate::task::Task;
+use crate::parse::parse;
+use crate::error::*;
+
+struct Content {
+    tasks: Vec<Task>
+}
+
+struct Database {
+    file_path: PathBuf,
+    head_id: Option<String>,
+    archives: Vec<Archive>,
+    content: Content,
+}
+
+impl Database {
+    pub fn new(path: PathBuf) -> Result<Database, Error> {
+        let archives = read_dir(&path)?
+            .filter(|p| {
+                p.is_ok() &&
+                p.as_ref().unwrap().path().to_str().unwrap().ends_with(".zip")
+            })
+            .map(|p| Archive::new(p.unwrap().path()))
+            .collect::<Result<Vec<Archive>, Error>>()?;
+
+        let mut db = Database {
+            file_path: path,
+            head_id: None,
+            archives,
+            content: Content {
+                tasks: Vec::new()
+            }
+        };
+
+        db.load_all();
+
+        Ok(db)
+    }
+
+    fn load_all(&mut self) {
+        // TODO
+    }
+
+    pub fn write(&mut self, delta: Content) -> Result<(), Error> {
+        let cur_head = self.head_id.as_ref()
+            .expect("attempted to write to db without loading it first");
+        let archive = Archive::save(cur_head, delta)?;
+        self.archives.push(archive);
+        self.head_id = Some(archive.id.into());
+        Ok(())
+    }
+
+    pub fn content(&self) -> &Content {
+        &self.content
+    }
+}
+
+struct Archive {
+    file_path: PathBuf,
+    date: String,
+    id: String,
+    parent_id: String,
+}
+
+impl Archive {
+    fn new(path: PathBuf) -> Result<Archive, Error> {
+        let path_parser =
+            Regex::new(r"^(\d{14})=([A-Za-z0-9\_\-]{11})-([A-Za-z0-9\_\-]{11})")
+            .unwrap();
+
+        let path_string: String = path.to_str().unwrap().into();
+        let caps = path_parser.captures(&path_string);
+
+        if let Some(caps) = caps {
+            Ok(Archive {
+                file_path: path,
+                date:      caps.get(1).unwrap().as_str().into(),
+                parent_id: caps.get(2).unwrap().as_str().into(),
+                id:        caps.get(3).unwrap().as_str().into(),
+            })
+        } else {
+            Err(Box::new(OpenFocusError::Parse))
+        }
+    }
+
+    fn save(parent_id: &str, delta: Content) -> Result<Archive, Error> {
+        unimplemented!()
+    }
+
+    fn read(&self) -> Result<Content, Error> {
+        // TODO: invoke parse.rs
+        unimplemented!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+mod parse;
 mod plist;
 
+pub mod db;
 pub mod task;
-pub mod parse;
 pub mod filter;
 pub mod error;
 pub mod perspective;
