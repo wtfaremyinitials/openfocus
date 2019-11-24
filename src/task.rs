cb7c6231584fd7e8079fca24e0e7f28fd1cbe8a2
Will Franzen <wtfranzen@gmail.com>
2019-11-23 23:19:07 -0700

    WIP id generation

--- src/task.rs
+++ src/task.rs
@@ -5,8 +5,16 @@ use crate::error::*;
 
 pub type ID = String;
 
+const alphabet: &'static [u8] =
+    b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-";
+
 fn generate_id() -> ID {
-    "x1234567890".into() // TODO
+    /*let mut s = String::new();
+    for i in 0..12 {
+        s.push(alphabet[rand::random::<u8>() % 54])
+    }
+    s*/
+    "1234567890_".into()
 }
 
 // enumeration of the order in which subtasks can be completed
